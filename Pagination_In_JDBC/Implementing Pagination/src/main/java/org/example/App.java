33f28370de48a5158a2ce35aa04eb9cfb2ec6425
Pravin-Sonwane-2004 <171813375+pravin-sonwane-2004@users.noreply.github.com>
2026-06-29 16:41:20 +0530

    Closion this project: enough

--- Pagination_In_JDBC/Implementing Pagination/src/main/java/org/example/App.java
+++ Pagination_In_JDBC/Implementing Pagination/src/main/java/org/example/App.java
@@ -1,13 +1,43 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/ind";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "0000";
+
+    public static void main(String [] args) throws SQLException {
+        int pageSize = 5;     // number of records per page
+        int pageNumber = 1;   // change this (1, 2, 3...)
+
+        int offset = (pageNumber - 1) * pageSize;
+
+        Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+
+        PreparedStatement ps = conn.prepareStatement(
+                "SELECT id, name, email FROM students LIMIT ? OFFSET ?"
+        );
+
+        ps.setInt(1, pageSize);
+        ps.setInt(2, offset);
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(rs.getInt("id"));
+            System.out.println(rs.getString("name"));
+            System.out.println(rs.getString("email"));
+            System.out.println("-------------------");
+        }
     }
 }
